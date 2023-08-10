d71610ea6e8bdbe28c03abe050b929d71716016c
Arshiagarg <arshia.garg@dxc.com>
2023-08-10 18:06:52 +0530

    updated angular

---
+++ angluar/online-banking/src/app/home/home.component.ts
@@ -0,0 +1,546 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgForm } from '@angular/forms';
+import { SharedDataService } from '../shared-data.service';
+
+interface LogoutResponse {
+  headers: any;
+  body: {
+    responseMsg: string;
+    token: string;
+  };
+  statusCode: string;
+  statusCodeValue: number;
+}
+
+interface TransferResponse {
+  headers: any;
+  body: {
+    responseMsg: string;
+    id: string;
+    obj: string;
+  };
+  statusCode: string;
+  statusCodeValue: number;
+}
+
+interface PayResponse {
+  headers: any;
+  body: {
+    responseMsg: string;
+    id: string;
+    obj: string;
+  };
+  statusCode: string;
+  statusCodeValue: number;
+}
+
+interface creditPayResponse {
+  headers: any;
+  body: {
+    responseMsg: string;
+    id: string;
+    obj: string;
+  };
+  statusCode: string;
+  statusCodeValue: number;
+}
+
+
+interface loanPayResponse {
+  headers: any;
+  body: {
+    responseMsg: string;
+    id: string;
+    obj: string;
+  };
+  statusCode: string;
+  statusCodeValue: number;
+}
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+
+  fromAccountNo: string = '';
+  mediumOfTrans: string = '';
+  transAmount: string = '';
+  toAccountNo: string = '';
+  comments: string = '';
+  MobNumber: string ='';
+  rechargeamount:string ='';
+  EMINumber:string ='';
+  LoanAmount:string ='';
+  CardNumber:string ='';
+  CVV:string ='';
+  ExpirationYear:string ='';
+  ExpirationMonth:string ='';
+  CardholderName:string ='';
+  accountnumber:string ='';
+  fromAccountNumber = '';
+ toAccountNumber:string = '';
+ //username:string = '';
+ amount = '';
+  //selectedTransactionType = '';
+  formComments = '';
+
+
+  constructor(
+    private http: HttpClient, 
+    private router: Router, 
+    private activatedRoute: ActivatedRoute,
+     private snackBar: MatSnackBar, 
+     private sharedDataService: SharedDataService)
+     
+     {
+    this.email = this.sharedDataService.getEmail();
+    this.username = this.sharedDataService.getUsername();
+    this.phoneNumber = this.sharedDataService.getPhoneNumber();
+    this.roleName = this.sharedDataService.getRoleName();
+  }
+  currentUserRole: string = '';
+  currentPage = 1; // Current page number
+  itemsPerPage = 5;
+  username: string = '';
+  email: string = '';
+  phoneNumber: string = '';
+  roleName: string = '';
+  loginTime: string = '';
+  currentTab: string = 'accounts';
+  selectedTransactionType: string = '';
+  selectedPaymentType: string = '';
+  selectedAccountType: string = '';
+  selectedAccount: any;
+  showHistory: boolean = false;
+  transactionHistory: any[] = [];
+  transfer: any = {};
+  Pay: any = {};
+  loanPay: any = {};
+  creditPay: any = {};
+  accountTypes: any[] = [];
+///defaultAccNo: string =localStorage.getItem('accNo');
+defaultAccNo: string ='1234567890';
+  accountNumbers: any[] = [];
+  accounts: any[] = [
+    {
+      accountNumber: '1234567890',
+      branch: 'Branch Name',
+      name: 'Srini Vastava',
+      balance: 49800
+    },
+    // Add more account details here
+  ];
+  showNewAccountForm: boolean = false;
+  newAccount: any = {
+    accountType: '',
+    branch: '',
+    name: '',
+    initialDeposit: 0
+  };
+
+  showProfileDetails: boolean = false;
+  hideProfileDetails() {
+    this.showProfileDetails = false;
+  }
+  ngOnInit() {
+    this.updateLoginTime();
+    this.getAccountTypes();
+  }
+
+  openNewAccountForm() {
+    this.redirectToNewAccountPage();
+  }
+  hideNewAccountForm() {
+    this.showNewAccountForm = false;
+  }
+  submitNewAccountForm() {
+
+  }
+
+
+  updateLoginTime() {
+    setInterval(() => {
+      const currentDate = new Date();
+      this.loginTime = this.formatTime(currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
+    }, 1000);
+  }
+
+  formatTime(hours: number, minutes: number, seconds: number): string {
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes.toString();
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds.toString();
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
+  }
+  selectTab(tabName: string) {
+    this.currentTab = tabName;
+
+  }
+
+  viewTransactionHistory() {
+
+    this.showHistory = true;
+    // Logic to fetch transaction history from the server and populate the transactionHistory array
+    // For demonstration purposes, let's assume we have some dummy data here
+    this.transactionHistory = [
+      { id: '764536', type: 'NEFT', date: '2023-07-01', description: 'Grocery Shopping', debit: 5200, credit: 0, amount: 49800 },
+      { date: '2023-06-28', description: 'Restaurant Bill', debit: 5000, credit: 0, amount: 55000 },
+      { date: '2023-06-25', description: 'Utility Payment', debit: 0, credit: 60000, amount: 60000 },
+      { date: '2023-06-25', description: 'Utility Payment', debit: 0, credit: 60000, amount: 60000 },
+      { date: '2023-06-25', description: 'Utility Payment', debit: 0, credit: 60000, amount: 60000 },
+
+    ];
+  }
+  getAccountTypes() {
+    this.http.get<any>('http://localhost:9092/transfer/accTypes/all', {}).subscribe(
+      response => {
+        if (response.statusCodeValue === 200) {
+          // Handle successful login response
+          this.accountTypes = response.body.obj;
+          this.accountTypes.unshift({ accTypeId: 0, accTypeName: '--Select--' });
+          this.selectedAccountType = '--Select--';
+          this.selectedAccountType = this.accountTypes[0].accTypeName;
+
+        }
+      },
+      error => {
+        console.error('Error fetching account types:', error);
+      }
+    );
+  }
+  onAccountTypeChange() {
+    if (this.selectedAccountType !== '--Select--') {
+      const selectedAccount = this.accountTypes.find(
+        account => account.accTypeName === this.selectedAccountType
+      );
+      const accTypeId = selectedAccount.accTypeId;
+      this.getAccountNumbersFromAPI(accTypeId);
+    } else {
+      this.accountNumbers = []; // Reset accountNumbers when the "Select Account Type" is chosen
+    }
+  }
+  getAccountNumbersFromAPI(accTypeId: number) {
+    this.http.get<any>(`http://localhost:9092/transfer/getAccNumbers?accTypeId=${accTypeId}`).subscribe(
+      response => {
+
+        this.accountNumbers = response.body.obj.map((account: any) => account.accNumber);
+      },
+      error => {
+        console.error('Error fetching account numbers:', error);
+      }
+    );
+  }
+
+ clearForm(transferForm: NgForm) {
+    transferForm.reset();
+    this.transfer = {}; // Reset the transfer object as well
+  }
+
+  transferMoney(transferForm: any) {
+    if (transferForm && transferForm.invalid) {
+      return;
+    }
+    const transferData = {
+      fromAccountNo: this.fromAccountNumber,
+      toAccountNo: this.toAccountNumber,
+      trans: this.amount,
+      mediumOfTrans: this.selectedTransactionType,
+      comments: this.formComments
+    };
+      console.log("TransData: ", transferData);
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+      this.http.post<TransferResponse>('http://localhost:9092/transfer/debit', transferData, { headers }).subscribe(
+        (response) => {
+          if (response.statusCodeValue === 200) {                  
+            console.log('Registration successful!', response.body.responseMsg);
+            this.displaySuccessMessage(response.body.responseMsg);
+            this.transfer = {};
+            this.fromAccountNo = '';
+        this.mediumOfTrans = '';
+        this.transAmount = '';
+        this.toAccountNo = '';
+        this.comments = '';
+            this.currentTab = 'accounts';
+            this.selectTab(this.currentTab);
+          }
+        },
+        (error) => {
+
+          console.error('Registration failed!', error.error.responseMsg);
+          if (error.status === 400) {
+            if (error.error && error.error.responseMsg) {
+              this.displayLoginMessage(error.error.responseMsg, 'red');
+            } else {
+              this.displayLoginMessage('Unauthorized: Invalid email or password', 'red');
+            }
+          } else if (error.status === 404) {
+            this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+          } else {
+            this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+          }
+          // Handle the error response from the backend API
+          // You can display an error message or perform other actions here
+        }
+      );
+    
+  }
+
+  logout() {
+
+    this.http.post<LogoutResponse>('http://localhost:9091/user/signout', {}).subscribe(
+      response => {
+        if (response.statusCodeValue === 200) {
+          // Handle successful login response
+          console.log('Login Successful:', response.body.responseMsg);
+          this.displaySuccessMessage(response.body.responseMsg);
+          this.redirectToLoginPage();
+        }
+      },
+      error => {
+        // Handle login error
+        console.error('Login Error:', error);
+        if (error.status === 400) {
+          if (error.error && error.error.responseMsg) {
+            this.displayLoginMessage(error.error.responseMsg, 'red');
+          }
+        } else if (error.status === 404) {
+          this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+        } else {
+          this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+        }
+      }
+    );
+  }
+  viewUserProfileDetails() {
+    const url = `http://localhost:9091/user/getUserByName?username=${encodeURIComponent(this.email)}`;
+    this.http.get<any>(url).subscribe(
+      response => {
+        if (response.statusCodeValue === 200) {
+          console.log('User Info:', response.body.userName);
+          this.phoneNumber = response.body.phoneNumber;
+          this.showProfileDetails = true;
+
+        }
+
+      },
+      error => {
+        // Handle login error
+        console.error('Login Error:', error);
+        if (error.status === 400) {
+          if (error.error && error.error.responseMsg) {
+            this.displayLoginMessage(error.error.responseMsg, 'red');
+          } else {
+            this.displayLoginMessage('Unauthorized: Invalid email', 'red');
+          }
+        } else if (error.status === 404) {
+          this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+        } else {
+          this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+        }
+      }
+    );
+  }
+  redirectToLoginPage() {
+    this.router.navigate(['/login']);
+  }
+  displaySuccessMessage(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      verticalPosition: 'top',
+      duration: 3000, // Adjust the duration as needed
+      panelClass: ['success-snackbar'] // Add custom CSS class for styling
+    });
+  }
+  displayLoginMessage(message: string, color: string) {
+    const loginMessage = document.getElementById('loginMessage');
+    if (loginMessage) {
+      loginMessage.textContent = message;
+      loginMessage.style.color = color;
+    }
+  }
+  redirectToNewAccountPage() {
+    this.router.navigate(['/account']);
+  }
+
+
+  
+  payNow(payform
+    : any ) {
+
+    // Perform form validation
+    if (payform && payform.invalid) {
+      return;
+    }
+    const payData = {
+      accountumber :this.Pay.accountnumber,
+      rechargeamount : this.Pay.amount,
+       MobNumber : this.Pay.mobileNumber,
+    };
+
+    console.log("payNow: ", payData);
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+
+      this.http.post<PayResponse>('http://localhost:9092/transfer/debit', payData, { headers }).subscribe(
+        (response) => {
+          if (response.statusCodeValue === 200) {                  
+            console.log('Registration successful!', response.body.responseMsg);
+            this.displaySuccessMessage(response.body.responseMsg);
+            this.Pay = {};
+        this.MobNumber = '';
+        this.rechargeamount = '';
+        this.accountnumber ='';
+            this.currentTab = 'billPayRecharge';
+            this.selectTab(this.currentTab);
+          }
+        },
+        (error) => {
+
+          console.error('Bill Pay failed!', error.error.responseMsg);
+          if (error.status === 400) {
+            if (error.error && error.error.responseMsg) {
+              this.displayLoginMessage(error.error.responseMsg, 'red');
+            } else {
+              this.displayLoginMessage('Unauthorized: mobile number', 'red');
+            }
+          } else if (error.status === 404) {
+            this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+          } else {
+            this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+          }
+          // Handle the error response from the backend API
+          // You can display an error message or perform other actions here
+        }
+      );
+    }
+
+    loan_Pay(loanpayform
+      : any ) {
+  
+      // Perform form validation
+      if (loanpayform && loanpayform.invalid) {
+        return;
+      }
+      const loanpayData = {
+        acnumber: this.loanPay.accountnumber,
+        loan_amount: this.loanPay.Amount,
+        lnumber : this.loanPay.emiNumber,
+        
+      };
+  
+      console.log("loanPay: ", loanpayData);
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json'
+        });
+
+
+        this.http.post<loanPayResponse>('http://localhost:8050/loanService/payment', loanpayData, { headers }).subscribe(
+        (response) => {
+          if (response.statusCodeValue === 200) {                  
+            console.log(' Payment successful!', response.body.responseMsg);
+            this.displaySuccessMessage(response.body.responseMsg);
+            this.loanPay = {};
+        this.LoanAmount = '';
+        this.accountnumber ='';
+        this. EMINumber = '';
+            this.currentTab = 'billPayRecharge';
+            this.selectTab(this.currentTab);
+          }
+        },
+        (error) => {
+
+          console.error('Bill Pay failed!', error.error.responseMsg);
+          if (error.status === 400) {
+            if (error.error && error.error.responseMsg) {
+              this.displayLoginMessage(error.error.responseMsg, 'red');
+            } else {
+              this.displayLoginMessage('Unauthorized: mobile number', 'red');
+            }
+          } else if (error.status === 404) {
+            this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+          } else {
+            this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+          }
+          // Handle the error response from the backend API
+          // You can display an error message or perform other actions here
+        }
+      );
+      }
+
+
+      CreditCard_Pay(creditPayform
+        : any ) {
+    
+        // Perform form validation
+        if (creditPayform && creditPayform.invalid) {
+          return;
+        }
+        const creditpayData = {
+          accountNumber: this.defaultAccNo,
+          cvv: this.creditPay.cvv,
+          creditCardNumber : this.creditPay.cardnumber,
+          CardholderName :this.creditPay. cardholdername,
+          expiryMonth:this.creditPay.expirationmonth,
+          expiryYear:this.creditPay.expirationyear,
+          
+        };
+    
+        console.log("creditPay: ", creditpayData);
+          const headers = new HttpHeaders({
+            'Content-Type': 'application/json'
+          });
+
+
+          this.http.post<creditPayResponse>('http://localhost:8089/api/creditCardPymt', creditpayData, { headers }).subscribe(
+            (response) => {
+              if (response.statusCodeValue === 200) {                  
+                console.log('Payment successful!', response.body.responseMsg);
+                this.displaySuccessMessage(response.body.responseMsg);
+                this.CardNumber = '';
+                this.CardholderName = '';
+                this.ExpirationMonth = '';
+                this.ExpirationYear = '';
+                this.CVV = '';
+                this.currentTab = 'billPayRecharge';
+                this.selectTab(this.currentTab);
+              }
+            },
+            (error) => {
+    
+              console.error('Bill Pay failed!', error.error.responseMsg);
+              if (error.status === 400) {
+                if (error.error && error.error.responseMsg) {
+                  this.displayLoginMessage(error.error.responseMsg, 'red');
+                } else {
+                  this.displayLoginMessage('Unauthorized: Card_Number', 'red');
+                }
+              } else if (error.status === 404) {
+                this.displayLoginMessage('Not Found: API endpoint not found', 'red');
+              } else {
+                this.displayLoginMessage('An error occurred. Please try again later.', 'red');
+              }
+              // Handle the error response from the backend API
+              // You can display an error message or perform other actions here
+            }
+          );
+
+
+          
+        }
+
+getAccountInfo(){
+  const accountNumber =localStorage.getItem('accountNumber') ??'';
+}
+
+        
+  }
+    
